689bba95406617a185d794e2795cc48b9697d32e
AlexisEche <alexis.eche58@gmail.com>
2021-03-03 18:51:26 -0500

    Create heroku repository

--- catalog_courses_backend/urls.py
+++ catalog_courses_backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from api import views
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register('courses',views.CourseViewSet)
@@ -12,4 +14,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include(router.urls))
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
