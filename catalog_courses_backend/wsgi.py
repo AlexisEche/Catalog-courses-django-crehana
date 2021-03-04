5175d454e41a07dff40f6fe9424abc570e2c126c
AlexisEche <alexis.eche58@gmail.com>
2021-03-03 22:43:49 -0500

    Change the name project in procfile

--- catalog_courses_backend/wsgi.py
+++ catalog_courses_backend/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for catalog_courses_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
 
 import os
 
